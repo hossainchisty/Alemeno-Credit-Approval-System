902c8ef039a4bb68512fafd90d0b1524d36473bb
hossainchisty <hossain.chisty11@gmail.com>
2024-02-04 23:12:39 +0600

    chore: Added production db

--- config/settings.py
+++ config/settings.py
@@ -83,22 +83,15 @@ APPEND_SLASH=False
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'credit_approval_system_dten',
-#        'USER': 'credit_approval_system_dten_user',
-#        'PASSWORD': 'wWabR6dEVQNMMw9KOAkq643GjJgGWEfy',
-#        'HOST': 'dpg-cmv5jb7109ks73b92h3g-a',
-#        'PORT': '5432',
-#    }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "./credit-db.sqlite3",
-    },
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'credit_approval_system_dten',
+       'USER': 'credit_approval_system_dten_user',
+       'PASSWORD': 'wWabR6dEVQNMMw9KOAkq643GjJgGWEfy',
+       'HOST': 'dpg-cmv5jb7109ks73b92h3g-a',
+       'PORT': '5432',
+   }
 }
 
 # Password validation
